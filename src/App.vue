918b0a9f527560bf73d309829672509d8e479acc
刘伟奇 <13619255270@163.com>
2024-04-28 23:36:14 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>hi</h1>
+  <Frank></Frank>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
