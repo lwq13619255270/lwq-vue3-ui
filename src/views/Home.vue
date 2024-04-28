3157123bdc809dc5ffe81040333bbe31c6c0447a
刘伟奇 <13619255270@163.com>
2024-04-29 00:29:11 +0800

    首页搭建

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="top-nav">
+            <div class="log">LOGO</div>
+            <ul class="menu">
+                <li></li>
+            </ul>
+        </div>
+        <div class="banner">
+            <h1>又又UI</h1>
+            <h2>一个基于vue3+vite+typescript开发的一个UI框架</h2>
+            <p class="actions">
+                <a>GitHub</a>
+                <a>开始</a>
+            </p>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .top-nav {
+        border: 1px solid red
+    }
+</style>
