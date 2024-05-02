9e0108e0386821b0e781ca9870095f21d052fd2d
刘伟奇 <13619255270@163.com>
2024-05-02 13:55:35 +0800

    切换路由后关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,16 @@
 
 <script lang="ts">
 import { provide, ref } from 'vue'
+import { router } from './router'
 export default {
   name: 'App',
   setup() {
     const width = document.documentElement.clientWidth;
     const asideVisible = ref(width <= 500 ? false : true);
-    provide('asideVisible', asideVisible)
+    provide('asideVisible', asideVisible);
+    router.afterEach((to, from) => {
+      asideVisible.value = false;
+    })
   }
 }
 </script>
