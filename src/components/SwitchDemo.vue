e0967d40e8bd0aae493ad66960ebbce900f05507
刘伟奇 <13619255270@163.com>
2024-05-04 00:06:49 +0800

    父级控制switch的开关

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="switch">
-    <Switch></Switch>
+    <Switch :value="checked" @input=" checked = $event"></Switch>
   </div>
 </template>
 <script lang="ts">
+import { ref } from 'vue';
 import Switch from '../lib/Switch.vue';
 export default {
   name: 'SwitchDemo',
   components: {
     Switch
+  },
+  setup() {
+    const checked = ref(false);
+    return {
+      checked
+    }
   }
 }
 </script>
