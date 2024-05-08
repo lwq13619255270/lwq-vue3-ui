4a6708061f5769a166946116fb3573327142a458
刘伟奇 <13619255270@163.com>
2024-05-08 23:26:25 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    dialog 组件
+    <!-- 
+      需求
+      1、点击后弹出
+      2、有遮罩层overlay
+      3、有close按钮
+      4、有标题
+      5、有内容
+      6、有yes/no 按钮
+     -->
+     <Dialog></Dialog>
   </div>
 </template>
 <script lang="ts">
+import Dialog from '../lib/Dialog.vue';
+
 export default {
-  name: 'DialogDemo'
+  name: 'DialogDemo',
+  components: {
+    Dialog
+  }
 }
 </script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>内容</p>
+    </main>
+    <footer>
+        <Button>OK</Button>
+        <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+<script lang="ts">
+import Button from './Button.vue';
+
+export default {
+  name: 'Dialog',
+  components: {
+    Button
+  }
+}
+</script>
