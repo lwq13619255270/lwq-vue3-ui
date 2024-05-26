b3d11c28f5e6b904a99795203afe5845ef8f6cb2
刘伟奇 <13619255270@163.com>
2024-05-26 23:56:30 +0800

    点击切换内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,13 +2,14 @@
   <div>
     tabs 组件
     <h1>示例1</h1>
-    <Tabs>
+    <Tabs v-model:value="name">
       <Tab title="导航1">tab1</Tab>
       <Tab title="导航2">tab2</Tab>
     </Tabs>
   </div>
 </template>
 <script lang="ts">
+import { ref } from 'vue';
 import Tab from '../lib/Tab.vue';
 import Tabs from '../lib/Tabs.vue';
 export default {
@@ -16,6 +17,12 @@ export default {
   components: {
     Tabs,
     Tab
+  },
+  setup(){
+    const name = ref('导航1');
+    return  {
+      name
+    }
   }
 }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,60 +1,96 @@
 <template>
   <div class="lwq-tabs">
     <div class="lwq-tabs-nav">
-        <div class="lwq-tabs-nav-item" v-for="(t, index) in titles" :key="index">
-            {{ t }}
-        </div>
+      <div
+        class="lwq-tabs-nav-item"
+        :class="{ selected: t === value }"
+        @click="select(t)"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+      <div class="lwq-tabs-nav-indicator"></div>
     </div>
     <div class="lwq-tabs-content">
-        <component class="lwq-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index" />
+      <component
+        class="lwq-tabs-content-item"
+        :is="current"
+        :key="current.props.title"
+      />
     </div>
-</div>
+  </div>
 </template>
 <script lang="ts">
-import Tab from './Tab.vue';
+import { computed } from "vue";
+import Tab from "./Tab.vue";
 
 export default {
+  props: {
+    value: {
+      type: String,
+    },
+  },
   setup(props, ctx) {
-    console.log(ctx)
+    console.log(ctx);
     const defaults = ctx.slots.default?.();
-    const titles: string[] = [];
-    defaults?.forEach(tab => {
+    defaults?.forEach((tab) => {
       if (tab.type !== Tab) {
-        throw new Error('Tabs 子标签必须是 Tab')
-      } 
-      console.log(tab)
-      titles.push(tab.props?.title)
-    })
+        throw new Error("Tabs 子标签必须是 Tab");
+      }
+      console.log(tab);
+      // titles.push(tab.props?.title)
+    });
+    const titles = defaults?.map((tab) => {
+      return tab.props?.title;
+    });
+    const select = (title: string) => {
+      ctx.emit("update:value", title);
+    };
+    const current = computed(() => {
+      return defaults?.find((tab) => tab.props?.title === props.value);
+    });
     return {
       defaults,
-      titles
-    }
-  }
-}
+      titles,
+      current,
+      select,
+    };
+  },
+};
 </script>
 <style lang="scss">
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
 .lwq-tabs {
-    &-nav {
-        display: flex;
-        color: $color;
-        border-bottom: 1px solid $border-color;
-        &-item {
-            padding: 8px 0;
-            margin: 0 16px;
-            cursor: pointer;
-            &:first-child {
-                margin-left: 0;
-            }
-            &.selected {
-                color: $blue;
-            }
-        }
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
     }
-    &-content {
-        padding: 8px 0;
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
     }
+  }
+  &-content {
+    padding: 8px 0;
+  }
 }
 </style>
